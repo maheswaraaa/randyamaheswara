34be252ae6c3543c725ba13b82ae69b7f968dd63
Randya Maheswara <randyamaheswara@rocketmail.com>
2026-02-03 01:42:30 +0700

    update tagline and insert wa number

--- content/profile.ts
+++ content/profile.ts
@@ -1,14 +1,15 @@
 export const profile = {
   name: "Randya Maheswara",
   role: "Software Engineer",
-  tagline: "Designing and building thoughtful software experiences",
+  tagline: "Discover. Design. Deliver.",
   bio: "Software engineer with a strong focus on ideation, structure, and problem framing — before code.",
   location: "Yogyakarta,Indonesia",
   image: "/images/profile-pic.jpg",
   email: "randyamaheswara.work@gmail.com",
   socials: {
     github: "https://github.com/maheswaraaa",
-    linkedin: "https://www.linkedin.com/in/randyamaheswara/",
+    linkedIn: "https://www.linkedin.com/in/randyamaheswara/",
+    whatsApp: "https://wa.me/6285330559588",
     "twitter / x": "https://x.com/madudidot",
   },
 };
