cf4237f09ab7a865a21f5c151e27f229101bf559
Randya Maheswara <randyamaheswara@rocketmail.com>
2026-02-06 01:12:10 +0700

    update icon

---
+++ src/app/icon.tsx
@@ -0,0 +1,39 @@
+import { ImageResponse } from "next/og";
+
+// Route segment config
+export const runtime = "edge";
+
+// Image metadata
+export const size = {
+    width: 512,
+    height: 512,
+};
+export const contentType = "image/png";
+
+// Image generation
+export default function Icon() {
+    return new ImageResponse(
+        (
+            <div
+                style={{
+                    fontSize: 350,
+                    background: "#1c1d20",
+                    width: "100%",
+                    height: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "#f5f5f5",
+                    borderRadius: 128,
+                    fontFamily: "sans-serif",
+                    fontWeight: "bold",
+                }}
+            >
+                R
+            </div>
+        ),
+        {
+            ...size,
+        }
+    );
+}
